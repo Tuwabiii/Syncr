1e1d6501c559da35b6ce274d57418156cf9f3a18
Christopher Harvey Mercado <145651069+Tuwabiii@users.noreply.github.com>
2023-12-09 09:45:14 +0800

    Add files via upload

---
+++ home.py
@@ -0,0 +1,5 @@
+import random
+
+texts = ["''In every barangay, let's work together for \n a brighter future by embracing the Sustainable Development Goals.''", "''Small steps in barangays lead to big \n changes for our world—let's support the SDGs for a better tomorrow.''", "''Barangays are the grassroots of change; let's unite to achieve \n the Sustainable Development Goals for a sustainable community.''", "''In each barangay, let's turn the SDGs into action, creating a ripple effect \n of positive change throughout our communities.''"]
+random_text = random.choice(texts)
+pyscript.write("text1", random_text)
